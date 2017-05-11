d7c7563c13e7beb07f0bf4906a74299222dda118
Krzysztof Wlazło <krzysztofwlazlo1993@gmail.com>
2017-05-11 16:52:25 +0200

    I have implemented abstractdao and added CmakeLists to our project

---
+++ tsp_core/src/dao/abstractdao.h
@@ -0,0 +1,13 @@
+#ifndef ABSTRACT_DAO_H_
+#define ABSTRACT_DAO_H_
+
+public class TaskResult
+{
+    virtual std::vector<TaskResult> getAllTaskResults() = 0;
+    virtual TaskResult getTaskResult(std::string token) = 0;
+    virtual void createTaskResult(TaskResult taskResult) = 0;
+    virtual void updateTaskResult(TaskResult taskResult) = 0;
+    virtual void deleteTaskResult(TaskResult taskResult) = 0;
+}
+
+#endif // ABSTRACT_DAO_H_
